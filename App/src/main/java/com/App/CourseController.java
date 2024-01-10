6baef5940bba8c5b4feafd1cb72dbe35991764d5
Mobin Arshad <mobink09@gmail.com>
2024-01-11 00:01:39 +0530

    Simple Rest API in spring boot

---
+++ App/src/main/java/com/App/CourseController.java
@@ -0,0 +1,20 @@
+package com.App;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+   
+	@RequestMapping("/course")
+	public List<Course> getCourses(){
+		return Arrays.asList(
+				new Course(1, "Full Stack Development", "in28Minutes"),
+				new Course(2, "Microservices with Spring Boot", "in28Minutes"),
+				new Course(3, "Learn AWS", "in28Minutes")
+				);
+	}
+}

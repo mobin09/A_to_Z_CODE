41d958c2ece4cbd652025c0a1d88dcfe9f718c42
Mobin Arshad <mobink09@gmail.com>
2024-02-01 21:38:43 +0530

    Day3

---
+++ DSA/Schooling/UpperCaseConversion.java
@@ -0,0 +1,25 @@
+package Schooling;
+
+import java.util.Arrays;
+
+public class UpperCaseConversion {
+
+    public static  String capital(String str){
+        String s = str.substring(0,1).toUpperCase()+str.substring(1);
+        return s;
+    }
+
+
+    public static void main(String[] args){
+        String str = "i love programming";
+        String[] s = str.split(" ");
+        //System.out.println(Arrays.toString(s));
+        String fString = "";
+        for(int i=0; i<s.length; i++){
+            fString += capital(s[i]) + " ";
+        }
+       
+        System.out.println(fString);
+
+    }
+}

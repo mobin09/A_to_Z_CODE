37f37da9c767ac7ed88b9fa328fe7b5127b76e74
Mobin Arshad <mobink09@gmail.com>
2024-02-14 23:19:07 +0530

    H2

--- learn-jpa-hibernate/src/main/java/com/springBoot/learnjpahibernate/LearnJpaHibernateApplication.java
+++ learn-jpa-hibernate/src/main/java/com/springBoot/learnjpahibernate/LearnJpaHibernateApplication.java
@@ -9,5 +9,5 @@ public class LearnJpaHibernateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnJpaHibernateApplication.class, args);
 	}
-
+	//url=jdbc:h2:mem:6939f67f-6401-46af-8906-2c8f640631bc user=SA
 }

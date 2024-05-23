8ce460c51bd485984fae72260e8997def37161e6
Mobin Arshad <mobink09@gmail.com>
2024-05-23 23:20:53 +0530

    i18n

--- restfull-web-services/src/main/java/com/demo/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
+++ restfull-web-services/src/main/java/com/demo/rest/webservices/restfullwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,18 @@
 package com.demo.rest.webservices.restfullwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -21,6 +28,14 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	
+	@GetMapping(path="/hello-world-internationalization")
+	public String helloWorldInteri18n() {
+		Locale locale = LocaleContextHolder.getLocale();
+		String retValue = messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		return retValue;
 	}
 
 }

736321be47f5118a62224bac06cd781197605d26
Mobin Arshad <mobink09@gmail.com>
2024-05-24 00:25:09 +0530

    String code

--- Array/Str0008.java
+++ Array/Str0008.java
@@ -1,6 +1,11 @@
 public class Str0008 {
     public static void main(String[] args) {
         String[] word = {"Hello", "I", "am", "Learning", "Java"};
-        System.out.println(String.join("",word));
+       // System.out.println(String.join("",word));
+       String key = "the quick brown fox jumps over the lazy dog";
+      key =  key.replace(" ","");
+      System.out.println(key);
+      System.out.println((int)'a' + " " + (int)'z');
+
     }   
 }

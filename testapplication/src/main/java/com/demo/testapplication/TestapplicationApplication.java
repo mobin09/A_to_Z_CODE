e40dd8c89113743bb4027b4fd09e38301df657f7
Mobin Arshad <mobink09@gmail.com>
2024-09-27 21:45:09 +0530

    FIrst commit

---
+++ testapplication/src/main/java/com/demo/testapplication/TestapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.testapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestapplicationApplication.class, args);
+	}
+
+}
---
+++ testapplication/src/test/java/com/demo/testapplication/TestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.testapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
